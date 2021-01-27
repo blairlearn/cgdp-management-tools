47f2fb9632d0604ce4cfd67527310e8359acce8c
Blair Learn <blairlearn@users.noreply.github.com>
2021-01-27 16:29:14 -0500

    Set domains after staging.
    - Read target environment from command line.
    - Apply naming conventions to derive lower tier domain names.
    - Add jest to dev dependencies.

--- index.js
+++ index.js
@@ -2,18 +2,19 @@ const config = require('config');
 const SiteFactoryClient = require('./lib/site-factory-client');
 const SiteFactoryClient2 = require('./lib/site-factory-client-2');
 const fs = require('fs');
-
-// TODO: This needs to come from the command-line.
-const TARGET_ENV = "dev";
+const DomainUtility = require('./lib/domain-utility');
+const { resolve } = require('path');
 
 const factoryConn = config.get('factoryConnection');
 
+// Client for v1 of the site factory API.
 const sourceClient = new SiteFactoryClient({
     username: factoryConn.username,
     apikey: factoryConn.apikey,
     factoryHost: factoryConn.factoryHost
 });
 
+// Client for v2 of the site factory API.
 const sourceClient2 = new SiteFactoryClient2({
     username: factoryConn.username,
     apikey: factoryConn.apikey,
@@ -22,80 +23,114 @@ const sourceClient2 = new SiteFactoryClient2({
 
 async function main() {
     try {
+        const targetEnv = getTargetEnvironment();
+        if (targetEnv !== 'dev' && targetEnv !== 'test') {
+            console.error('You must specify a staging target, either "DEV" or "TEST".');
+            process.exit(1);
+        }
+
         const srcSitelist = await sourceClient.sites.list(); // List of ACSF sites
 
         const siteIds = srcSitelist.map((site) => site.id);
 
         // Stage to requested lower tier.
-        let stageTask = await sourceClient2.stage.stage(TARGET_ENV, siteIds, true, true, false);
+        let stageTask = await sourceClient2.stage.stage(targetEnv, siteIds, true, true, false);
 
         // Wait for staging to complete.
         await sourceClient.tasks.waitForCompletion(stageTask.task_id, showProgress);
 
-        console.log(JSON.stringify(stageTask));
-        console.log(`Done staging to '${TARGET_ENV}' environment.`);
+        // Get client for target server.
+        const targetServer = DomainUtility.SFToStagingTarget(factoryConn.factoryHost, targetEnv);
+        const destClient = new SiteFactoryClient({
+            username: factoryConn.username,
+            apikey: factoryConn.apikey,
+            factoryHost: targetServer
+        });
 
         // Re-assign domains to the target.
+
+        // Get the domain information for each site
+        const srcDomainList = await getDomainList(sourceClient, siteIds)
+        const destDomainList = DomainUtility.GenerateDestinationDomains(srcDomainList, targetEnv);
+
+        await doDomainAssignments(destClient, destDomainList);
+
+        console.log(`Done staging to '${targetEnv}' environment.`);
 
     } catch (err) {
         console.error(err);
     }
 }
 
+/**
+ * Callback function for updating progress of the staging operation.
+ *
+ * @param {Boolean} isComplete Pass true if the operation has completed, false otherwise.
+ */
 function showProgress(isComplete) {
     process.stdout.write('.');
-    if(isComplete){
+    if (isComplete) {
         process.stdout.write('\n');
     }
 }
 
-async function doAssignments(sitelist, domainlist) {
-
-    sitePromises = [];
-    sitelist.forEach(site => {
-        sitePromises += doPerSiteAssignments(site.id, domainlist[site.site]);
-    });
-    await Promise.all(sitePromises);
-}
-
-async function doPerSiteAssignments(siteId, domainInfo) {
-
-    // Make sure the default domain is assinged first.
-    await sourceClient.domains.add(siteId, domainInfo.default);
-
-    // Set the additonal domains
-    additionalSites = [];
-    domainInfo.additional.forEach(domain => {
-        additionalSites += sourceClient.domains.add(siteId, domain);
-    });
-
-    await Promise.all(additionalSites);
+/**
+ * Parse the command line to find out the name of the environment to stage.
+ */
+function getTargetEnvironment() {
+    const rawArgs = process.argv;
+    if (rawArgs.length > 2 && typeof (rawArgs[2]) === "string") {
+        return rawArgs[2].toLowerCase();
+    }
+    else {
+        return null;
+    }
 }
 
 /**
- * Retrieve a list of the domains which are mapped to each
- * of the ACSF sites.
+ * Retrieve all the domain information objects for a list of site IDs.
  *
- * {
- *   "host": {
- *     "default": "default.cancer.gov",
- *     "additional": [
- *       "name1.cancer.gov",
- *       "name2.cancer.gov"
- *     ]
- *   }
- * }
- *
+ * @param {SiteFactoryClient} sfClient Client connected to the staging origin server.
+ * @param {Int32Array} siteIDList An array of the sites.
  */
-async function getDomains() {
-    return new Promise((resolve, reject) => {
-        fs.readFile('domainlist.json', function read(err, data) {
-            if (err)
-                reject(err);
-            else
-                resolve(JSON.parse(data));
-        });
-    });
+async function getDomainList(sfClient, siteIDList) {
+    return Promise.all(
+        siteIDList.map(
+            async (id) => await sfClient.domains.get(id)
+        )
+    );
 }
+
+/**
+ * Perform the actual domain assignments.
+ *
+ * @param {SiteFactoryClient} sfClient Client connected to staging destination server.
+ * @param {Array} assignments array of domain structures.
+ */
+async function doDomainAssignments(sfClient, assignments) {
+
+    /**
+     *  Note: Individual entries in the assignments array have the structure
+     *      {
+     *          siteID: 123,
+     *          primary: 'site1-test-acsf.example.com',
+     *          secondary: [
+     *              "site1-cms-test.example.com"
+     *          ]
+     *      }
+     */
+    return Promise.all(assignments.map(async (domain) => {
+        await sfClient.domains.add(domain.siteID, domain.primary);
+        Promise.all(
+                domain.secondary.map(async (siteAlias) => {
+                    await sfClient.domains.add(domain.siteID, siteAlias);
+            })
+        );
+    }))
+
+}
+
+
+
 
 main();
